2b3e897db6e1898c1dc2993dff21efa7e0e491f5
Maik Freitas <maikemir2003@gmail.com>
2021-01-19 14:27:39 -0300

    jogo estilizado e finalizado

--- js/script.js
+++ js/script.js
@@ -39,10 +39,19 @@ function update (event){
 }
 
 function iniciarJogo(){
+
     if(snake[0].x > 15 * box && direct == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direct == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direct == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direct == "up") snake[0].y = 16 * box;
+
+    for(i = 1;i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game over :(");
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
